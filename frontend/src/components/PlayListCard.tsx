9deb9803d53bf453286ab86098d0ead3edb1ef64
Arpit9026 <arpitkairati54@gmail.com>
2025-08-30 09:51:18 +0530

    Username in PlayList Card

--- frontend/src/components/PlayListCard.tsx
+++ frontend/src/components/PlayListCard.tsx
@@ -1,6 +1,8 @@
 import { FaMusic } from "react-icons/fa"
+import { useUserData } from "../context/UserContext"
 
 const PlayListCard = () => {
+  const {user,isAuth} = useUserData();
   return (
     <div className="flex items-center p-4 rounded-lg shadow-md gap-4 cursor-pointer hover:bg-[#ffffff28]">
       <div className="w-10 h-10 bg-gray-600 flex items-center justify-center rounded-md">
@@ -8,7 +10,7 @@ const PlayListCard = () => {
       </div>
       <div className="ml-4">
         <h2>My PlayList</h2>
-        <p className="text-gray-400 text-sm"> PlayList • <span>{"User"}</span></p>
+        <p className="text-gray-400 text-sm"> PlayList • {isAuth ? <span>{user?.name}</span> :<span>{"User"}</span>}</p>
       </div>
     </div>
   )
